342ecbf517c25fe4ff7a62cea49139144355e85e
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2019-08-19 13:31:24 +0300

    Ignore pending channels

--- feereport.py
+++ feereport.py
@@ -29,26 +29,28 @@ def feereport(plugin=None):
     channels = rpc.listfunds()["channels"]
     channel_fees = []
     for channel in channels:
-        channel_detail = rpc.listchannels(channel["short_channel_id"])["channels"]
-        for detail in channel_detail:
-            if detail["source"] == our_nodeid:
+        # pending channels will not have short_channel_id, ignore them
+        if "short_channel_id" in channel:
+            channel_detail = rpc.listchannels(channel["short_channel_id"])["channels"]
+            for detail in channel_detail:
+                if detail["source"] == our_nodeid:
 
-                if "funding_output" in channel:
-                    funding_output = str(channel["funding_output"])
-                else:
-                    # funding_output isn't returned in v0.7.1, but we can get it
-                    # from short_channel_id too
-                    short_channel_id_parts = channel["short_channel_id"].split("x")
-                    funding_output = short_channel_id_parts[2]
+                    if "funding_output" in channel:
+                        funding_output = str(channel["funding_output"])
+                    else:
+                        # funding_output isn't returned in v0.7.1, but we can get it
+                        # from short_channel_id too
+                        short_channel_id_parts = channel["short_channel_id"].split("x")
+                        funding_output = short_channel_id_parts[2]
 
-                channel_fees.append({
-                    "chan_point": channel["funding_txid"] + ":" + funding_output,
-                    "base_fee_msat": str(detail["base_fee_millisatoshi"]),
-                    "fee_per_mil": str(detail["fee_per_millionth"]),
-                    "fee_rate": "%.8f" % float(detail["fee_per_millionth"] / 1000000)
-                })
+                    channel_fees.append({
+                        "chan_point": channel["funding_txid"] + ":" + funding_output,
+                        "base_fee_msat": str(detail["base_fee_millisatoshi"]),
+                        "fee_per_mil": str(detail["fee_per_millionth"]),
+                        "fee_rate": "%.8f" % float(detail["fee_per_millionth"] / 1000000)
+                    })
 
-                break
+                    break
 
     fee_data = ((fwd["fee"], fwd["resolved_time"])
                 for fwd in rpc.listforwards()["forwards"]
